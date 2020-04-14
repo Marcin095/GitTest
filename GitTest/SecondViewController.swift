6af98e8a6ec262a25b6efc88cf53edc90416300d
Marcin Kaliniak <marcin.kaliniak@eleader.biz>
2020-04-14 10:38:02 +0200

    Add secondViewController

---
+++ GitTest/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  GitTest
+//
+//  Created by Marcin Kaliniak on 14/04/2020.
+//  Copyright © 2020 Marcin Kaliniak. All rights reserved.
+//
+
+import Foundation
